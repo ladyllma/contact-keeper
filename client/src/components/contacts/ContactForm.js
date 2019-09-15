a6562b2a0cb984420b53d6ae06fbafc9779d7ab5
LadyMA <leidylaurama@gmail.com>
2019-09-14 22:55:08 -0400

    Implementing updateContact, deleteContact, setCurrent and clearCurrent methods in the ContactContext

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,115 @@
+import React, { useState, useContext, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactForm = () => {
+    const contactContext = useContext(ContactContext);
+    const { addContact, updateContact, clearCurrent, current } = contactContext;
+
+    const [contact, setContact] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        type: 'Personal'
+    });
+
+    useEffect(() => {
+        if (current !== null) {
+            setContact(current);
+        } else {
+            clearFields();
+        }
+    }, [contactContext, current]);
+
+    const { name, email, phone, type } = contact;
+
+    const clearFields = () => {
+        setContact({
+            name: '',
+            email: '',
+            phone: '',
+            type: 'Personal'
+        });
+    };
+
+    const clearAll = () => {
+        clearCurrent();
+    };
+
+    const onChange = ({ target }) =>
+        setContact({ ...contact, [target.name]: target.value });
+
+    const onSubmit = event => {
+        event.preventDefault();
+        if (current === null) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
+        clearAll();
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h2 className="text-primary">
+                {current ? 'Update Contact' : 'Add Contact'}
+            </h2>
+            <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={onChange}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={onChange}
+            />
+            <input
+                type="text"
+                placeholder="Phone"
+                name="phone"
+                value={phone}
+                onChange={onChange}
+            />
+            <h5>Contact type</h5>
+            <input
+                type="radio"
+                name="type"
+                value="Personal"
+                checked={type === 'Personal'}
+                onChange={onChange}
+            />{' '}
+            Personal{' '}
+            <input
+                type="radio"
+                name="type"
+                value="Professional"
+                checked={type === 'Professional'}
+                onChange={onChange}
+            />{' '}
+            Professional{' '}
+            <div>
+                <input
+                    type="submit"
+                    value="Save"
+                    className="btn btn-primary btn-block"
+                />
+            </div>
+            {current && (
+                <div>
+                    <button
+                        className="btn btn-light btn-block"
+                        onClick={clearAll}
+                    >
+                        Clear
+                    </button>
+                </div>
+            )}
+        </form>
+    );
+};
+
+export default ContactForm;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,15 +36,43 @@ const ContactState = props => {
                 phone: '121-131-1441',
                 type: 'Professional'
             }
-        ]
+        ],
+        current: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    };
+
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    };
+
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    };
+
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact });
+    };
+
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT });
+    };
+
     return (
         <ContactContext.Provider
             value={{
-                contacts: state.contacts
+                contacts: state.contacts,
+                current: state.current,
+                addContact,
+                updateContact,
+                deleteContact,
+                setCurrent,
+                clearCurrent
             }}
         >
             {props.children}
