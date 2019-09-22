8069f68e55ab2baa51e68806b58b0dfb071eefd0
LadyMA <leidylaurama@gmail.com>
2019-09-21 22:04:06 -0400

    Setting things up in order to deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
@@ -13,5 +14,14 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// SERVE STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+   // SET STATIC FOLDER
+   app.use('client/build');
+   app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
