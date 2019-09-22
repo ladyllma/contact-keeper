863d2493cb79bb98fc43d2dd57e642437ec06e51
LadyMA <leidylaurama@gmail.com>
2019-09-21 22:52:19 -0400

    Fixing error in server.js file causing error when app was deployed

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use('/api/auth', require('./routes/auth'));
 // SERVE STATIC ASSETS IN PRODUCTION
 if (process.env.NODE_ENV === 'production') {
    // SET STATIC FOLDER
-   app.use('client/build');
+   app.use(express.static('client/build'));
    app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    );
 }
 
-app.listen(process.env.PORT || 5000, () =>
-   console.log(`Server started on port ${PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
